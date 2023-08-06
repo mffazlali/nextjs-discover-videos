165dd06dc43e16900b3fad83a94b7441de3d1fad
mohammad.Fazlali <mohammad.fazlali@pnr.ir>
2023-08-06 14:37:27 +0330

    add sectionCards

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,20 +1,44 @@
+'use client'
+import { useState } from 'react'
 import styles from './card.module.css'
 import Image from 'next/image'
+import { motion } from 'framer-motion'
+import cls from 'classnames'
 
 const Card = (props: any) => {
-  const { imgUrl, size } = props
+  const {
+    imgUrl = 'https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1159&q=80',
+    size = 'medium',
+    id,
+  } = props
   const classMap = {
     small: styles.smItem,
     medium: styles.mdItem,
     large: styles.lgItem,
   }
+
+  const [imgSrc, setImgSrc] = useState(imgUrl)
+  const scale = id === 0 ? { scaleY: 1.1 } : { scale: 1.1 }
+  const onErrorHandler = () => {
+    setImgSrc(
+      'https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1159&q=80'
+    )
+  }
+
   return (
     <div className={styles.container}>
-      <div className={styles.imageWrapper}>
-        <div className={Object(classMap)[size]}>
-          <Image src={imgUrl} fill={true} alt=""></Image>
-        </div>
-      </div>
+      <motion.div
+        className={cls(styles.imageWrapper, Object(classMap)[size])}
+        whileHover={scale}
+      >
+        <Image
+          onError={onErrorHandler}
+          src={imgSrc}
+          fill={true}
+          alt=""
+          unoptimized={true}
+        ></Image>
+      </motion.div>
     </div>
   )
 }
---
+++ src/components/card/section-cards.tsx
@@ -0,0 +1,18 @@
+'use client'
+import Card from './card'
+import styles from './section-cards.module.css'
+
+const SectionCards = (props: any) => {
+  const { title, videos, size } = props
+  return (
+    <section className={styles.container}>
+      <h3 className={styles.title}>{title}</h3>
+      <div className={styles.cardWrapper}>
+        {[...videos].map((video, idx) => {
+          return <Card imgUrl={video.imgUrl} size={size} id={idx} key={idx} />
+        })}
+      </div>
+    </section>
+  )
+}
+export default SectionCards
