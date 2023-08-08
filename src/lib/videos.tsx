8d1ce3cc054b5e1830aaa10416762fd574651e3e
mohammad.Fazlali <mohammad.fazlali@pnr.ir>
2023-08-08 17:31:48 +0330

    add api youtube

--- src/lib/videos.tsx
+++ src/lib/videos.tsx
@@ -1,6 +1,11 @@
 import videoData from '../store/videos.json'
-export const getVideos = () => {
-  const videos = [...videoData.items].map((video) => {
+export const getVideos = async(search:string) => {
+  const youtubeAPIKey = process.env.YOUTUBE_API_KEY
+  const input=`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${search}&key=${youtubeAPIKey}`
+  const response=await fetch(input)
+  const jsonResponse=await response.json()
+
+  const videos = [...jsonResponse.items].map((video) => {
     return {
       id: video.id.videoId,
       imgUrl: video.snippet.thumbnails.high.url,
