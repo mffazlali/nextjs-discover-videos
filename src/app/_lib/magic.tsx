76d1f5fc86cd1675cf107cf5e26a6af4a28716c6
mohammad.Fazlali <mohammad.fazlali@pnr.ir>
2023-08-19 08:32:47 +0330

    edit magic

--- src/app/_lib/magic.tsx
+++ src/app/_lib/magic.tsx
@@ -1,7 +1,6 @@
 import { Magic } from 'magic-sdk'
 
 const createMagic = () => {
-  // pk_live_646746B6BC3A7FF1
   const publicKey = process.env.NEXT_PUBLIC_MAGIC_PUBLISHABLE_API_KEY!
   return typeof window !== 'undefined' ? new Magic(publicKey) : null
 }
