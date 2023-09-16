82d445d7984f30d5db67efe570b6bc9cb967a8d3
mohammad fazlali <58655751+mffazlali@users.noreply.github.com>
2023-09-16 10:25:58 -0700

    edit hasura

--- src/app/_lib/hasura.tsx
+++ src/app/_lib/hasura.tsx
@@ -3,15 +3,16 @@ This is an example snippet - you should consider tailoring it
 to your service.
 */
 
-async function fetchGraphQL(
+async function queryHusaraGQL(
   operationsDoc: string,
   operationName: string,
   variables: any
 ) {
-  const result = await fetch('https://optimal-drum-65.hasura.app/v1/graphql', {
+  const result = await fetch(process.env.NEXT_PUBLIC_HUSARA_ADIMN_URL!, {
     method: 'POST',
     headers: {
-      'x-hasura-admin-secret': 'iX24GlcsLpslFbKpKcJlSqc0sbgYOj5m4SCqcbHJ2d42Et6ypQDHaIs8HaiUNaT0',
+      // 'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HUSARA_ADIMN_SECRET!,
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiZmF6bGFsaSIsImlhdCI6MTY5NDg4NDIxNCwiZXhwIjoxNjk1NDg5MDY3LCJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWRlZmF1bHQtcm9sZSI6InVzZXIiLCJ4LWhhc3VyYS1hbGxvd2VkLXJvbGVzIjpbInVzZXIiLCJhZG1pbiJdLCJ4LWhhc3VyYS11c2VyLWlkIjoicmV6YWllIn19.nqANpFQ5ek0RmE_UfraxbFOstnuXdHzUS_LzTmFVSi4',
     },
     body: JSON.stringify({
       query: operationsDoc,
@@ -23,7 +24,8 @@ async function fetchGraphQL(
   return await result.json()
 }
 
-const operationsDoc = `
+function fetchMyQuery() {
+  const operationsDoc = `
     query MyQuery {
       users {
         issuer
@@ -33,9 +35,7 @@ const operationsDoc = `
       }
     }
   `
-
-function fetchMyQuery() {
-  return fetchGraphQL(operationsDoc, 'MyQuery', {})
+  return queryHusaraGQL(operationsDoc, 'MyQuery', {})
 }
 
 export async function startFetchMyQuery() {
